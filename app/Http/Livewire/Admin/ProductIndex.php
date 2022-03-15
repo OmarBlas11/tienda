4b4a912a8bd5c1743945d9b4043c4ec3d19ee397
OmarBlas11 <loarteblas@gail.com>
2022-03-15 18:38:17 -0500

    modal for create products

--- app/Http/Livewire/Admin/ProductIndex.php
+++ app/Http/Livewire/Admin/ProductIndex.php
@@ -6,6 +6,8 @@ use App\Models\Category;
 use App\Models\Product;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Str;
+use Ramsey\Uuid\Type\Decimal;
 
 class ProductIndex extends Component
 {
@@ -13,6 +15,13 @@ class ProductIndex extends Component
     public $search;
     public $paginar;
     public $category;
+    public $date;
+    public $name;
+    public $slug;
+    public $precio_compra;
+    public $precio_venta;
+    public $stock;
+    public $category_id;
     protected $paginationTheme = 'bootstrap';
     public function updatingSearch()
     {
@@ -27,24 +36,59 @@ class ProductIndex extends Component
         $this->resetPage();
     }
     public function render()
-    {   
+    {
         if ($this->category) {
-            $products = Product::where('category_id',$this->category)->orderby('id','desc')->paginate($this->paginar);
-        }else{
-            $products = Product::where('id','LIKE','%'.$this->search.'%')
-            ->orwhere('name','LIKE','%'.$this->search.'%')
-            ->orwhere('precio_compra','LIKE','%'.$this->search.'%')
-            ->orwhere('precio_venta','LIKE','%'.$this->search.'%')
-            ->orwhere('stock','LIKE','%'.$this->search.'%')->orderby('id','desc')->paginate($this->paginar);
+            $products = Product::where('category_id', $this->category)->orderby('id', 'desc')->paginate($this->paginar);
+        } else {
+            $products = Product::where('id', 'LIKE', '%' . $this->search . '%')
+                ->orwhere('name', 'LIKE', '%' . $this->search . '%')
+                ->orwhere('precio_compra', 'LIKE', '%' . $this->search . '%')
+                ->orwhere('precio_venta', 'LIKE', '%' . $this->search . '%')
+                ->orwhere('stock', 'LIKE', '%' . $this->search . '%')->orderby('id', 'desc')->paginate($this->paginar);
         }
         $categories = Category::all();
         return view('livewire.admin.product-index', compact('products', 'categories'));
+    }
+    protected $rules = [
+        'name' => 'required',
+        'slug' => 'required|unique:products,slug',
+        'precio_venta' => 'required|numeric|regex:/^[\d]{0,5}(\.[\d]{1,2})?$/',
+        'precio_compra' => 'required|numeric|regex:/^[\d]{0,5}(\.[\d]{1,2})?$/',
+        'stock' => 'required|numeric',
+        'category_id' => 'required',
+    ];
+    protected $messages = [
+        'name.required' => 'El nombre no debe quedar vacio',
+        'slug.required'=> 'El nombre no debe quedar vacio',
+        'precio_compra.required' => 'El precio de compra no debe quedar vacio',
+        'precio_compra.numeric' => 'El percio de compra debe ser un numero',
+        'precio_compra.regex' => 'El precio debe contener como maximo 5 numeros enteros y 2 decimales',
+        'precio_venta.required' => 'El precio de venta no debe quedar vacio',
+        'precio_venta.numeric' => 'El percio de venta debe ser un numero',
+        'precio_venta.regex' => 'El precio de venta debe contener como maximo 5 numeros enteros y 2 decimales',
+        'stock.required'=> 'La cantidad no debe quedar vacio',
+        'stock.numeric' => 'La cantidad se numerico',
+        'category_id.required' => 'La categoria no debe quedar vacio',
+    ];
+    public function updated($label)
+    {
+        $this->validateOnly($label, $this->rules, $this->messages);
+        
+    }
+    public function store()
+    {
+        $this->slug = Str::slug($this->name);
+        $this->validate();
     }
     public function editar()
     {
         # code...
     }
     public function delete()
+    {
+        # code...
+    }
+    public function cancel()
     {
         # code...
     }
--- resources/views/livewire/admin/product-index.blade.php
+++ resources/views/livewire/admin/product-index.blade.php
@@ -1,6 +1,9 @@
 <div>
     <div class="card">
         <div class="card-header">
+            <a class="btn btn-secondary" data-toggle="modal" data-target="#ventana_create">Agregar Nuevo Producto</a>
+        </div>
+        <div class="card-header">
             <div class="row form-inline">
                 <div class="col-7">
                     <div class="input-group mb-1">
@@ -8,8 +11,7 @@
                             <span class="input-group-text" id="inputGroup-sizing-lg">Buscar</span>
                         </div>
                         <input type="text" wire:model="search" class="form-control" aria-label="Default"
-                            aria-describedby="inputGroup-sizing-default"
-                            placeholder="Ingresar el dato que sea buscar">
+                            aria-describedby="inputGroup-sizing-default" placeholder="Ingresar el dato que sea buscar">
                     </div>
                 </div>
                 <div class="col">
@@ -36,15 +38,28 @@
                         </div>
                     </div>
                 </div>
-                {{$category}}
+                {{ $date }}
+                <div class="col">
+                    <div class="input-group">
+                        <div class="col">
+                            <div class="input-group mb-1">
+                                {{-- <div class="input-group-prepend">
+                                    <span class="input-group-text" id="inputGroup-sizing-lg">Buscar</span>
+                                </div> --}}
+                                <input type="date" class="form-control" wire:model="date" aria-label="Default"
+                                    aria-describedby="inputGroup-sizing-default">
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div class="col">
                     <div class="input-group">
                         <div class="col">
                             <select class="custom-select" id="paginarahora" wire:model="category">
                                 <option selected value="">Categorias</option>
-                               @foreach ($categories as $category)
-                               <option selected value="{{$category->id}}">{{$category->name}}</option>
-                               @endforeach
+                                @foreach ($categories as $category)
+                                    <option selected value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
                             </select>
                         </div>
                     </div>
@@ -78,7 +93,8 @@
                                     data-target="#ventana_edit" class="btn btn-primary btn-sm">Editar</a>
                             </td>
                             <td width="10px">
-                                <a id="btneDelete" wire:click="delete({{ $product->id }})"class="btn btn-danger btn-sm">Eliminar</a>
+                                <a id="btneDelete" wire:click="delete({{ $product->id }})"
+                                    class="btn btn-danger btn-sm">Eliminar</a>
                             </td>
                         </tr>
                     @endforeach
@@ -86,5 +102,70 @@
             </table>
         </div>
         <div class="card-footer table-responsive">{{ $products->onEachSide(2)->links() }}</div>
+    </div>
+    <div wire:ignore.self class="modal fade" tabindex="-1" id="ventana_create">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-center bg-center">Nueva Producto</h5>
+                    <a wire:click="cancel()" data-dismiss="modal" class="fas fa-times-circle" aria-label="Close"></a>
+                    </button>
+                </div>
+                @if (session()->has('info'))
+                    <span class="text-primary text-center p-3 mb-2 bg-warning">{{ session('info') }}</span>
+                @endif
+                <div class="modal-body">
+                    <form id="formulario" wire:submit.prevent="store">
+                        <div class="form-group mb-3 " id="inputname">
+                            <label class="form-label" for="name">Nombre</label>
+                            <input type="text" wire:model="name" type="text" class="form-control" id="name"
+                                autocomplete="off" placeholder="ingresar el nombre">
+                            @error('slug')
+                                <span class="error text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="precio_compra" class="form-label">Precio de Compra</label>
+                            <input type="number" step="any" name="precio_compra" placeholder="Ingresar el precio de compra" class="form-control" wire:model="precio_compra" autocomplete="off">
+                            @error('precio_compra')
+                                <span class="error text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="percio_venta" class="form-label">Precio de Venta</label>
+                            <input type="number" step="any" name="precio_venta" wire:model="precio_venta" class="form-control" placeholder="Ingresar el precio de venta" autocomplete="off">
+                            @error('precio_venta')
+                                <span class="error text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="stock" class="form-label">Cantidad</label>
+                            <input type="number" name="stock" wire:model="stock" class="form-control" placeholder="Ingresar la cantidad del producto" autocomplete="off">
+                            @error('stock')
+                                <span class="error text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="category_id">Categoria</label>
+                            <select class="custom-select" id="paginarahora" wire:model="category_id">
+                                <option selected value="">Categorias</option>
+                                @foreach ($categories as $category)
+                                    <option selected value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('category_id')
+                                <span class="error text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        {{-- <input type="text" wire:model="slug" type="text" name="name" hidden class="form-control" id="slug"
+                            placeholder="ingresar el nombre"> --}}
+                        <div class="modal-footer">
+                            <button wire:click="cancel()" data-dismiss="modal" class="btn btn-secondary">Cancelar</button>
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
